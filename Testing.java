86e66653df7ad410fad9059753cca7d5f0189c4d
Noel Khalaf <noel_khalaf@hotmail.com>
2019-06-06 14:08:28 -0400

    All

--- Testing.java
+++ Testing.java
@@ -3,11 +3,11 @@ public class Testing
 	public static void main(String[] args)
 	{
 		PrivateCar car = new PrivateCar(20,"Uber","Airport");
-<<<<<<< Updated upstream
-
-=======
+		Airfare plane = new Airfare(200,"Jet","Paris");
+		Break
 		
->>>>>>> Stashed changes
 		System.out.println("Amount for car: "+car.getAmount());
+		System
+		System.out.println("Amount for plane: "+plane.getAmount());
 	}
 }
