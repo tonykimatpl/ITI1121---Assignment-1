f7b376e96e4c9d6cbcdb1ff7d9efc6ed7ec21e96
Noel Khalaf <noel_khalaf@hotmail.com>
2019-06-06 15:43:06 -0400

    Account

---
+++ Part 2/Account.java
@@ -0,0 +1,17 @@
+public abstract class Account{
+	private static lastAccountNumber = 9999;
+	private double balance;
+	Customer customer;
+	
+	public Account(){
+		
+		
+		this.accountNumber = lastAccountNumber;
+		lastAccountNumber ++;
+	}
+	public void reallocate(){
+		for(){
+			
+		}
+	}
+}
