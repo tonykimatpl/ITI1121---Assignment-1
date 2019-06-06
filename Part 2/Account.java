fc61138846fa632b542f3d8c07c3a23598b46c22
Tony Kim <tonykimatpl@gmail.com>
2019-06-06 16:26:43 -0400

    Account.java

--- Part 2/Account.java
+++ Part 2/Account.java
@@ -1,17 +1,48 @@
 public abstract class Account{
 	private static lastAccountNumber = 9999;
+  private Customer customer;
 	private double balance;
-	Customer customer;
-	
-	public Account(){
-		
-		
+  private Transaction[] transactions;
+	private Transaction newTransArray;
+  private static final int INITSIZE = 25;
+
+
+	public Account(Customer person, double bal){
+    this.balance = bal;
+    this.customer = person;
 		this.accountNumber = lastAccountNumber;
 		lastAccountNumber ++;
+    Transaction[] transactions = new Transaction[INITSIZE];
 	}
+
 	public void reallocate(){
-		for(){
-			
-		}
+    for(int i = 0; i < this.transactions.length;i++){
+      if(this.transactions[i] == null){
+        break;
+      }
+      else{
+        var = this.transactions.length;
+        Transaction[] newTransArray = new Transactions[var*2];
+        for(int i=0; i < var;i++){
+          newTransArray[i] = this.transactions[i];
+        }
+        this.transactions = newTransArray;
+      }
+    }
 	}
+  public double getBalance(){
+    return this.balance;
+  }
+  public Customer getCustomer(){
+    return this.customer;
+  }
+  public void setCustomer(Customer other){
+    this.customer = other;
+  }
+  public String toString(){
+    String a = "Account balance is: "+ this.balance+". Account Number is: "+ this.accountNumber+". ";
+    String b = this.getCustomer();
+    return a,b;
+
+  }
 }
