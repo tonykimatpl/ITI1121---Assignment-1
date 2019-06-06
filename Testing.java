e93bea30efa6bc28f8142bff234e4f6161f9da9c
Tony Kim <tonykimatpl@gmail.com>
2019-06-06 14:03:02 -0400

    testing

--- Testing.java
+++ Testing.java
@@ -3,7 +3,7 @@ public class Testing
 	public static void main(String[] args)
 	{
 		PrivateCar car = new PrivateCar(20,"Uber","Airport");
-		
-		System.out.println("Amount for car: "car.getAmount());
+
+		System.out.println("Amount for car: "+car.getAmount());
 	}
 }
