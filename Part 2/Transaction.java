c70bb6e78f0282834e578ec631f84af1dedaf7db
Tony Kim <tonykimatpl@gmail.com>
2019-06-06 18:11:09 -0400

    Transactions edit

--- Part 2/Transaction.java
+++ Part 2/Transaction.java
@@ -1,17 +1,39 @@
+import java.util.Date;
 public class Transaction{
   private byte type;
   private double amount;
+  private Date date;
+  private double fees;
+  private String description;
 
-  public String processTransaction(byte operation, double amt){
+  public Transaction(byte operation, double amt, Date datum, double fee, String desc){
+    this.type = operation;
+    this.amount = amt;
+    this.date = datum;
+    this.fees = fee;
+    this.description = desc;
+  }
+
+  public Transaction(byte operation, double amt, double fee, String desc){
+    java.util.Date currentdate = new java.util.Date();
+    this.date = currentdate;
+    this.type = operation;
+    this.amount = amt;
+    this.fees = fee;
+    this.description = desc;
+  }
+
+
+  public String processTransaction(){
     String transactionInfo;
-    if(operation == 0){
-      transactionInfo = "Deposit of amount: "+amt;
+    if(this.type) == 0){
+      transactionInfo = "Deposit of amount: "+this.amount;
     }
-    else if(operation == 1){
-      transactionInfo = "Withdrawal of amount: "+amt;
+    else if(this.type == 1){
+      transactionInfo = "Withdrawal of amount: "+this.amount;
     }
-    else if(operation == 2){
-      transactionInfo = "Added Interest of amount: "+amt;
+    else if(this.type == 2){
+      transactionInfo = "Added Interest of amount: "+this.amount;
     }
     return transactionInfo;
   }
