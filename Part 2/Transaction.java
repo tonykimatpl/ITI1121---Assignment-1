410a215df5e1556c777cf91678c0eb7ac2966ba9
Tony Kim <tonykimatpl@gmail.com>
2019-06-06 16:41:16 -0400

    Transaction process edit

--- Part 2/Transaction.java
+++ Part 2/Transaction.java
@@ -2,16 +2,16 @@ public class Transaction{
   private byte type;
   private double amount;
 
-  public String processTransaction{
+  public String processTransaction(byte operation, double amt){
     String transactionInfo;
     if(this.type == 0){
-      transactionInfo = "Deposit of amount: "+this.amount;
+      transactionInfo = "Deposit of amount: "+amt;
     }
     else if(this.type == 1){
-      transactionInfo = "Withdrawal of amount: "+this.amount;
+      transactionInfo = "Withdrawal of amount: "+amt;
     }
     else if(this.type == 2){
-      transactionInfo = "Added Interest of amount: "+this.amount;
+      transactionInfo = "Added Interest of amount: "+amt;
     }
     return transactionInfo;
   }
