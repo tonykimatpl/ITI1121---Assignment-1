79b05cf9bb911dfa49c7c952c765f77b8d5e7322
Tony Kim <tonykimatpl@gmail.com>
2019-06-07 16:06:04 -0400

    ExpenseTracker

--- Part 1/ExpenseTracker.java
+++ Part 1/ExpenseTracker.java
@@ -1,8 +1,19 @@
 public class ExpenseTracker {
-	private [] expenses;
+	private Expense[] expenses;
 	private int size;
 	public ExpenseTracker(int capacity) {
-		;
+		expenses = new Expense[capacity];
 		size = 0;
 	}
+  public boolean add(Expense e){
+		expenses[size] = e;
+		return true;
+  }
+  public double getTotal(){
+    double total = 0;
+    for(int i=0; i <= size; i++){
+      total += expenses[i].getAmount();
+    }
+    return total;
+  }
 }
